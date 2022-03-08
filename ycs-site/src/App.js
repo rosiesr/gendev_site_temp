e8be7756141c75d01aa022d93f421d415303703b
Rosie Rothschild <rosierothschild@Rosies-MBP.wireless.yale.internal>
2022-03-07 19:37:23 -0500

    created react app

---
+++ ycs-site/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Yale Computer Society Site
+        </h1>
+        <a
+          className="App-link"
+          href="http://yalecompsociety.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn About YCS
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
